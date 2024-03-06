c7c0f241f1a48df7cb36045cbca0dbbd70c6a2f4
Nitishj2 <nitishjaiswal691@gmail.com>
2024-03-06 21:58:48 +0530

    First commit Crypto

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import {ThemeProvider} from "./context/ThemeContext";
+import Home from "./routes/Home";
+import Signin from './routes/Signin';
+import Signup from './routes/Signup';
+import Account from './routes/Account';
+
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import CoinPage from "./components/CoinPage";
+import Footer from "./components/Footer";
+function App() {
+   const [coins,setCoins] = useState([]);
+
+   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true&locale=en';
+
+   useEffect(()=>{
+     axios.get(url).then((response)=>{
+      setCoins(response.data);
+      console.log(response.data);
+     })
+   },[url])
+
+
+  return (
+     <ThemeProvider>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element = {<Home coins = {coins}/>} />
+          <Route path="/signin" element = {<Signin/>} />
+          <Route path="/signup" element = {<Signup/>} />
+          <Route path="/account" element = {<Account/>} />
+          <Route path='/coin/:coinId' element={<CoinPage />}>
+            <Route path=':coinId' />
+          </Route>
+
+         </Routes>
+         <Footer/>
+     </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/routes/Account.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Account = () => {
+  return (
+    <div>
+        <h1>Working in Progress...</h1>
+    </div>
+  )
+}
+
+export default Account
